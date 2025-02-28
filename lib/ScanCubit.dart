f3bbeb5afd6a3bb7c12eac1781634984be373b36
Miguel <mig.ruiz@gmail.com>
2025-02-28 14:29:44 +0000

    fetching dunnes

--- lib/ScanCubit.dart
+++ lib/ScanCubit.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: file_names
 
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 import 'ScanState.dart';
 
 class ScanCubit extends Cubit<ScanState> {
@@ -10,7 +14,21 @@ class ScanCubit extends Cubit<ScanState> {
     emit(ScanningState());
     
   }
-  void barcodeFound(String barcode) {
-    emit(BarcodeFoundState(barcode: barcode));
+  void barcodeFound(String barcode) async{
+    HapticFeedback.vibrate();
+    final barcodes = {
+      '5056175945542':'100806893',
+      '35354344961':'100279329',
+    };
+    final productId = barcodes[barcode];
+    if (productId == null) {
+      emit(BarcodeFoundState(barcode: 'NOT FOUND'));
+      return;
+    }
+
+    final response = await http.get(Uri.parse('https://storefrontgateway.dunnesstoresgrocery.com/api/stores/258/preview?q=$productId'));
+    final json = jsonDecode(response.body)  as Map<String, dynamic>;
+    final sd = json['products'][0]['name'];
+    emit(BarcodeFoundState(barcode: sd));
   }
 }
